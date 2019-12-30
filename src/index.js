5988f6332d851ab0440c9535f262cb0af935eacc
madebyjustinzhang <jzha365@uwo.ca>
2019-12-30 16:57:11 -0500

    a

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { BrowserRouter as Router } from ‘react-router-dom’;
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
 
